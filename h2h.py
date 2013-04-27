52e068d862533a18a9af2330e08a3e365fd3b4b1
Nathan Grigg <nathan@nathanamy.org>
2013-04-26 19:48:00 -0700

    Separate files

--- h2h.py
+++ h2h.py
@@ -6,49 +6,6 @@ import codecs
 # import cProfile
 from vimh2h import VimH2H
 
-DOC_BEGIN = r"""\documentclass{article}
-% \documentclass[a4paper]{article} %a4
-\usepackage{xcolor}
-\definecolor{i}{RGB}{0,137,139}
-\definecolor{t}{RGB}{250,0,250}
-\definecolor{h}{RGB}{164,32,246}
-\definecolor{k}{RGB}{106,89,205}
-\definecolor{e}{RGB}{0,0,255}
-\definecolor{s}{RGB}{106,89,205}
-\definecolor{o}{RGB}{46,139,87}
-\definecolor{c}{RGB}{165,42,42}
-\usepackage{xltxtra}
-\usepackage[colorlinks=true]{hyperref}
-\usepackage{fancyvrb}
-\usepackage{fontspec}
-\setromanfont{Avenir Next}
-\setmonofont{Source Code Pro}
-\usepackage[margin=1in]{geometry}
-% \usepackage[margin=0.9in]{geometry} % a4
-% \usepackage[margin=.4in,bottom=.8in,paperwidth=7.3in,paperheight=9.73in]{geometry} % ipad
-\usepackage{fancyhdr}
-\pagestyle{fancy}
-\fancyhf{}
-\renewcommand{\headrulewidth}{0in}
-\cfoot{\rightmark{} \textemdash{} \thepage}
-\renewcommand\ll[2]{\hyperlink{#1}{\si{#2}}}
-\newcommand\ld[2]{\hyperlink{#1}{\color{black}#2}}
-\newcommand\ls[2]{\hyperlink{#1}{\ss{#2}}}
-\newcommand\lo[2]{\hyperlink{#1}{\so{#2}}}
-\newcommand\lc[2]{\hyperlink{#1}{\sc{#2}}}
-\newcommand\st[1]{{\color{t}#1}}
-\newcommand\si[1]{{\color{i}#1}}
-\newcommand\sn[1]{{\color{n}#1}}
-\newcommand\sh[1]{{\color{h}#1}}
-\newcommand\sk[1]{{\color{k}#1}}
-\newcommand\se[1]{{\color{e}#1}}
-\renewcommand\ss[1]{{\color{s}#1}}
-\renewcommand\sn[1]{{\color{blue}#1}}
-\newcommand\so[1]{\textbf{\color{o}#1}}
-\renewcommand\sc[1]{\textbf{\color{c}#1}}
-\begin{document}\phantomsection{}
-"""
-
 SECTION_BEGIN = r"""\addcontentsline{toc}{%s}{%s}
 \markright{%s}
 \begin{Verbatim}[commandchars=\\\{\}]
@@ -78,7 +35,6 @@ is written by Nathan Grigg, based on the HTML conversion script written
 by Carlo Teubner.
 \end{minipage}
 \vspace{\stretch{1}}
-\end{document}
 """
 
 def slurp(filename):
@@ -94,8 +50,7 @@ def main():
     h2h.add_tags(slurp('doc/vim_faq.txt'))
 
     contents = slurp('contents.txt').split('\n')
-    fout = codecs.open('vimdoc.tex', 'w', 'utf-8')
-    fout.write(DOC_BEGIN)
+    fout = codecs.open('body.tex', 'w', 'utf-8')
 
     level = "chapter"
     for row in contents:
